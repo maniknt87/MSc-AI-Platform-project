7e1f442446edb7d5f367e31cfa6781a4f083157c
maniknt87 <vroy1187@gmail.com>
2026-08-11 18:15:24 +0530

    Add Azure Firewall policy rules

--- backend/terraform/modules/firewall-policy/main.tf
+++ backend/terraform/modules/firewall-policy/main.tf
@@ -7,3 +7,23 @@ resource "azurerm_firewall_policy" "this" {
 
   tags = var.tags
 }
+
+resource "azurerm_firewall_policy_rule_collection_group" "network" {
+  name               = "rcg-network-${lower(var.policy_name)}"
+  firewall_policy_id = azurerm_firewall_policy.this.id
+  priority           = 100
+
+  network_rule_collection {
+    name     = "allow-required-network"
+    priority = 100
+    action   = "Allow"
+
+    rule {
+      name                  = "allow-https-outbound"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.1.0.0/16", "10.2.0.0/16"]
+      destination_addresses = ["*"]
+      destination_ports     = ["443"]
+    }
+  }
+}
